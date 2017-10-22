1f65d3ed8eec7022f06e779002e7c43d9954eefa
John Garavito Suarez <jfreddy@br.ibm.com>
2017-10-22 00:17:13 -0200

    Describe specific user information

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
     const app = express();
     const server = require("http").createServer(app);
     const appPort = 6009;
-    const bulkSize = 10000;
+    const bulkSize = 1000000;
     const csv = {
         "delimiter": ",",
         "skipHeader": true,
@@ -27,29 +27,63 @@
             "state": 6
         }
     };
+    const daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
-    let length = 0;
     let blocks = 0;
     let documents = [];
     let document = {};
     let stayPoints = {};
+    let userFrequency = {};
+    let key = "";
     let keys = [];
+    let dayOfweek = 0;
+
+    function compareTime(a,b) {
+        if (new Date("01/01/90 "+a.time).getTime() < new Date("01/01/90 "+b.time))
+            return -1;
+        if (new Date("01/01/90 "+a.time).getTime() > new Date("01/01/90 "+b.time))
+            return 1;
+        return 0;
+    }
 
     transformStream.on("data", function(chunk){
         document = JSON.parse(chunk.toString());
         //length = Object.keys(stayPoints).length;
         if(document.state === "SP" && documents.length < bulkSize){
             documents.push(document);
-            stayPoints[document.id] = (stayPoints[document.id]) ? stayPoints[document.id] + 1 : 1;
+            dayOfweek = daysOfWeek.indexOf(document.wday.toLowerCase());
+            if(!stayPoints[document.id]){
+                stayPoints[document.id] = {};
+                stayPoints[document.id]["state"] = document.state;
+                stayPoints[document.id]["daysOfWeek"] = {   "0": {"frequency": 0, "coordinates": []},
+                                                            "1": {"frequency": 0, "coordinates": []},
+                                                            "2": {"frequency": 0, "coordinates": []},
+                                                            "3": {"frequency": 0, "coordinates": []},
+                                                            "4": {"frequency": 0, "coordinates": []},
+                                                            "5": {"frequency": 0, "coordinates": []},
+                                                            "6": {"frequency": 0, "coordinates": []}};
+            }
+
+            stayPoints[document.id]["daysOfWeek"][dayOfweek].frequency = stayPoints[document.id]["daysOfWeek"][dayOfweek].frequency + 1;
+            stayPoints[document.id]["daysOfWeek"][dayOfweek].coordinates.push({"lat": document.lat, "lng": document.lng, "time": document.time});
+            userFrequency[document.id] = (stayPoints[document.id]) ? stayPoints[document.id] + 1 : 1;
             //stayPoints[document.id] = document;
             //console.log(documents.length);
         }
         if (documents.length === bulkSize) {
             blocks++;
             keys = Object.keys(stayPoints);
-            console.log("block "+blocks+" documents: "+documents.length);
-            console.log("users "+keys.length);
-            console.log(stayPoints);
+            console.log("\nBlock "+blocks+" documents: "+documents.length);
+            console.log("Total users "+keys.length);
+            key = keys.indexOf("6dc43b2edaf144f7c0a209c2ce6288258f9aefdde8da93b1236310f8351c4c77f6719a0388ec99ecf0c206306a487119");
+
+            console.log("Users "+key);
+            for(let i in stayPoints[keys[key]]["daysOfWeek"]){
+                stayPoints[keys[key]]["daysOfWeek"][i].coordinates.sort(compareTime)
+            }
+            //console.log(keys[406]);
+
+            console.log(JSON.stringify(stayPoints["6dc43b2edaf144f7c0a209c2ce6288258f9aefdde8da93b1236310f8351c4c77f6719a0388ec99ecf0c206306a487119"], undefined, 2));
             this.pause();
 
             documents = [];
